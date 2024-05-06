c3bbca75bf3ffaa84cfa510884e94588f94861ef
drot2579 <154900509+drot2579@users.noreply.github.com>
2024-05-06 05:28:14 +0300

    first commit

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const elMap = [
     ["btnLinks","button.links"],
     ["links","div.links"],
     ["leftMenu",".leftMenu"],
-    ["leftMenuButton",".leftMenuButton"],
+    ["leftBarButton",".leftBarButton"],
     ["leftMenuCurtain",".leftMenuCurtain"],
-    ["topMenu",".topMenu"],
-    ["topMenuButton",".topMenuButton"],
+    ["topLinks",".topLinks"],
+    ["topLinksButton",".topLinksButton"],
 
 ]
 const el = Object.fromEntries(elMap.map( ([name,selector]) => [name ,document.querySelector(selector)] ))
@@ -15,16 +15,16 @@ const toggleHidden = (element) => {element.classList.toggle("hidden")}
 //     el.links.classList.toggle("close")
 // })
 
-el.leftMenuButton.addEventListener("click", (e) => {
-    el.leftMenu.classList.toggle("swiped")
-    el.leftMenuCurtain.classList.toggle("swiped")
-})
+// el.leftBarButton.addEventListener("click", (e) => {
+//     el.leftMenu.classList.toggle("swiped")
+//     el.leftMenuCurtain.classList.toggle("swiped")
+// })
 
-el.leftMenuCurtain.addEventListener("click", (e) => {
-    el.leftMenuCurtain.classList.add("swiped")
-    el.leftMenu.classList.add("swiped")
-})
+// el.leftMenuCurtain.addEventListener("click", (e) => {
+//     el.leftMenuCurtain.classList.add("swiped")
+//     el.leftMenu.classList.add("swiped")
+// })
 
-el.topMenuButton.addEventListener("click", (e) => {
-    el.topMenu.classList.toggle("shut")
-})
+// el.topLinksButton.addEventListener("click", (e) => {
+//     el.topLinks.classList.toggle("shut")
+// })
